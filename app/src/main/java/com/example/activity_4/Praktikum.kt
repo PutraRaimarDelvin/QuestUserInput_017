72f913b62982803b867ca6fd5a7119e08fae1fd7
PutraRaimarDelvin <delvinputra805@gmail.com>
2026-02-08 23:18:24 +0700

    Menambahkan warna dan style pada header

--- app/src/main/java/com/example/activity_4/Praktikum.kt
+++ app/src/main/java/com/example/activity_4/Praktikum.kt
@@ -5,7 +5,10 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import java.lang.reflect.Modifier
 
 
@@ -15,12 +18,19 @@ fun Praktikum() {
         Box(
             modifier = Modifier
                 .fillMaxWidth()
-                .height(100.dp),
+                .height(100.dp)
+                .background(Color.Blue),
             contentAlignment = Alignment.Center
         ) {
-            Text("Formulir Pendaftaran")
+            Text(
+                text = "Formulir Pendaftaran",
+                color = Color.White,
+                fontWeight = FontWeight.Bold,
+                fontSize = 22.sp
+            )
         }
     }
 }
+
 
 
