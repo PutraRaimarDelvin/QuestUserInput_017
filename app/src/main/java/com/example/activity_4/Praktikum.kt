02737d1e9a6c6a0f0c0376f282825fcfa43268fe
PutraRaimarDelvin <delvinputra805@gmail.com>
2026-02-08 23:20:29 +0700

    Menambahkan pilihan jenis kelamin

--- app/src/main/java/com/example/activity_4/Praktikum.kt
+++ app/src/main/java/com/example/activity_4/Praktikum.kt
@@ -1,36 +1,35 @@
 package com.example.activity_4
 
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.material3.OutlinedTextField
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-
+import androidx.compose.ui.Alignment
 
 
 @Composable
 fun Praktikum() {
-    var textNama by remember { mutableStateOf("") }
-    var textAlamat by remember { mutableStateOf("") }
+    var textJK by remember { mutableStateOf("") }
+    val gender = listOf("Laki-laki", "Perempuan")
 
     Column {
-        Text("Formulir Pendaftaran")
+        Text("Jenis Kelamin")
 
-        OutlinedTextField(
-            value = textNama,
-            onValueChange = { textNama = it },
-            label = { Text("Nama Lengkap") }
-        )
-
-        OutlinedTextField(
-            value = textAlamat,
-            onValueChange = { textAlamat = it },
-            label = { Text("Alamat") }
-        )
+        gender.forEach {
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                RadioButton(
+                    selected = textJK == it,
+                    onClick = { textJK = it }
+                )
+                Text(it)
+            }
+        }
     }
 }
+
 
 
 
