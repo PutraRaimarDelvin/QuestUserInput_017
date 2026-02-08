688ed6fb3fef70c7e87c31c8e2754b0ddf6d00d3
PutraRaimarDelvin <delvinputra805@gmail.com>
2026-02-08 23:19:41 +0700

    Menambahkan state input nama dan alamat

--- app/src/main/java/com/example/activity_4/Praktikum.kt
+++ app/src/main/java/com/example/activity_4/Praktikum.kt
@@ -2,35 +2,37 @@ package com.example.activity_4
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import java.lang.reflect.Modifier
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+
 
 
 @Composable
 fun Praktikum() {
+    var textNama by remember { mutableStateOf("") }
+    var textAlamat by remember { mutableStateOf("") }
+
     Column {
-        Box(
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(100.dp)
-                .background(Color.Blue),
-            contentAlignment = Alignment.Center
-        ) {
-            Text(
-                text = "Formulir Pendaftaran",
-                color = Color.White,
-                fontWeight = FontWeight.Bold,
-                fontSize = 22.sp
-            )
-        }
+        Text("Formulir Pendaftaran")
+
+        OutlinedTextField(
+            value = textNama,
+            onValueChange = { textNama = it },
+            label = { Text("Nama Lengkap") }
+        )
+
+        OutlinedTextField(
+            value = textAlamat,
+            onValueChange = { textAlamat = it },
+            label = { Text("Alamat") }
+        )
     }
 }
+
+
 
 
 
