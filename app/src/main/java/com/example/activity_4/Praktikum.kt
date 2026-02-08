00f1a7a25d563c21a24da104d779a6acc505c9d7
PutraRaimarDelvin <delvinputra805@gmail.com>
2026-02-08 23:17:50 +0700

    Menambahkan header menggunakan Box

--- app/src/main/java/com/example/activity_4/Praktikum.kt
+++ app/src/main/java/com/example/activity_4/Praktikum.kt
@@ -1,14 +1,26 @@
 package com.example.activity_4
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.unit.dp
+import java.lang.reflect.Modifier
 
 
 @Composable
 fun Praktikum() {
     Column {
-        Text("Formulir Pendaftaran")
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(100.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            Text("Formulir Pendaftaran")
+        }
     }
 }
+
 
